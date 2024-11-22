75c30198d142673c463f37b601c5d903eba48941
nandemotoken <nandemotoken@gmail.com>
2024-11-22 22:46:05 +0900

    up

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-console.log('Web3Auth Client ID:', import.meta.env.VITE_WEB3AUTH_CLIENT_ID);
+// console.log('Web3Auth Client ID:', import.meta.env.VITE_WEB3AUTH_CLIENT_ID);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
