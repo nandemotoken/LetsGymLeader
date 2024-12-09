dab3829e843572925dffca0abbbb4aef2c8b98c6
nandemotoken <nandemotoken@gmail.com>
2024-12-10 00:50:31 +0900

    up gymleader admin front

--- gymleader/src/App.tsx
+++ gymleader/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { usePrivy, useWallets} from '@privy-io/react-auth';
 
@@ -27,21 +25,19 @@ function App() {
     }
   }
 
+  // 認証状態が変更されたら自動的にウォレット接続を実行
+  useEffect(() => {
+    if (authenticated && wallets.length > 0) {
+      connectWallet();
+    }
+  }, [authenticated, wallets]);
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>ジムリーダー専用画面</h1>
       
       {authenticated ? (
         <div>
-          <button onClick={connectWallet}>Connect Wallet</button>
           {address && <p>Wallet Address: {address}</p>}
           <button onClick={logout}>
             Logout
@@ -52,18 +48,6 @@ function App() {
           Login with discord
         </button>
       )}
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
