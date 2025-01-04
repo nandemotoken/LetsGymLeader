28fcceac41c4360857ad1e7c3a06538bbe25ab41
nandemotoken <nandemotoken@gmail.com>
2025-01-04 16:03:43 +0900

    adjusting router

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,20 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: {
-      rewrites: [
-        { 
-          from: /^\/howtouse/, 
-          to: '/howtouse/index.html'
-        },
-        {
-          from: /.*/,
-          to: '/index.html'
-        }
-      ]
-    }
-  },
   base: '/',
   define: {
     'process.env.VITE_WEB3AUTH_CLIENT_ID': JSON.stringify(process.env.VITE_WEB3AUTH_CLIENT_ID)
