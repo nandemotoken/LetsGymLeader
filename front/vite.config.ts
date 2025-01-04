a87a06a9d72a8ac4a3250e4b46ea46e5cad9154e
nandemotoken <nandemotoken@gmail.com>
2025-01-04 16:27:15 +0900

    adjust router

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        gymleader: './gymleader.html'
+      }
+    }
+  },
   define: {
     'process.env.VITE_WEB3AUTH_CLIENT_ID': JSON.stringify(process.env.VITE_WEB3AUTH_CLIENT_ID)
   }
