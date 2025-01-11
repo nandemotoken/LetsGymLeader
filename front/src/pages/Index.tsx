aeca8e5e9c3144ef3f356d1f0343aee584647e69
nandemotoken <nandemotoken@gmail.com>
2025-01-11 13:29:36 +0900

    delete time

--- front/src/pages/Index.tsx
+++ front/src/pages/Index.tsx
@@ -308,10 +308,10 @@ const MainContent: React.FC<MainContentProps> = ({ userInfo, obtainedBadges }) =
                 <User className="w-5 h-5 text-gray-400" />
                 <span>{selectedBadge.leader}</span>
               </div>
-              <div className="flex items-center space-x-2">
+              {/* <div className="flex items-center space-x-2">
                 <Clock className="w-5 h-5 text-gray-400" />
                 <span>{selectedBadge.hours}</span>
-              </div>
+              </div> */}
             </div>
 
             {obtainedBadges.has(selectedBadge.id) ? (
